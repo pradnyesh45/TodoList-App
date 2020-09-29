5ae46b1e5ab15e29bb36b0aa29e0902d0feb4d75
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2020-09-29 07:00:38 +0530

    Removing a Todo

--- ToDoList.js
+++ ToDoList.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { TodosContext } from "./App";
 import { SwipeListView } from "react-native-swipe-list-view";
 import { Text, Container, Header } from "native-base";
-import { StyleSheet, View } from "native-base";
+import { StyleSheet, TouchableOpacity, View } from "native-base";
 
 export default function ToDoList() {
   // receive state and dispatch from App.js
@@ -14,12 +14,29 @@ export default function ToDoList() {
     </View>
   );
 
+  const renderHiddenItem = (data, rowMap) => (
+    <View style={styles.rowBack}>
+      <TouchableOpacity>
+        <Text>Edit</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={[styles.backRightBtn]}>
+        <Text style={{ color: "#FFF" }}>Delete</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
   return (
     <Container>
       <Header>
         <Text>ToDoList</Text>
       </Header>
-      <SwipeListView data={state.todos} renderItem={renderItem} />
+      <SwipeListView
+        data={state.todos}
+        renderItem={renderItem}
+        renderHiddenItem={renderHiddenItem}
+        leftOpenValue={75}
+        rightOpenValue={-75}
+      />
     </Container>
   );
 }
@@ -31,5 +48,23 @@ const styles = StyleSheet.create({
     borderBottomWidth: 0.25,
     justifyContent: "center",
     height: 50,
+  },
+  rowBack: {
+    alignItems: "center",
+    backgroundColor: "#DDD",
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingLeft: 15,
+  },
+  backRightBtn: {
+    alignItems: "center",
+    bottom: 0,
+    justifyContent: "center",
+    position: "absolute",
+    top: 0,
+    width: 75,
+    backgroundColor: "red",
+    right: 0,
   },
 });
